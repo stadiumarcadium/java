c42d6869c36aadc8be331251efb2b9697c812ed9
StadiumArcadium <stdmarcdm@gmail.com>
2021-10-17 00:55:32 +0300

    lesson04 - hw - fixed

--- src/lesson04/TicTacToe.java
+++ src/lesson04/TicTacToe.java
@@ -1,9 +1,9 @@
+
 package lesson04;
 
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-
 
 public class TicTacToe {
     public static final char DOT_X = 'X';
@@ -12,8 +12,8 @@ public class TicTacToe {
     public static final int SIZE = 5;
     public static final int DOTS_TO_WIN = 4;
     public static char[][] map;
-    public static Scanner scanner;
-    public static Random random;
+    public static Scanner scanner = new Scanner (System.in);
+    public static Random random = new Random();
 
     public TicTacToe() {
     }
@@ -22,10 +22,10 @@ public class TicTacToe {
         initMap();
         printMap();
 
-        while(true) {
+        while (true) {
             humanTurn();
             printMap();
-            if (checkWin('X')) {
+            if (checkWin(DOT_X)) {
                 System.out.println("Победил человек");
                 break;
             }
@@ -37,7 +37,7 @@ public class TicTacToe {
 
             aiTurn();
             printMap();
-            if (checkWin('O')) {
+            if (checkWin(DOT_O)) {
                 System.out.println("Победил Искуственный Интеллект");
                 break;
             }
@@ -52,11 +52,11 @@ public class TicTacToe {
     }
 
     private static void initMap() {
-        map = new char[5][5];
+        map = new char[SIZE][SIZE];
 
-        for(int i = 0; i < 5; ++i) {
-            for(int j = 0; j < 5; ++j) {
-                map[i][j] = 8226;
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                map[i][j] = DOT_EMPTY;
             }
         }
 
@@ -64,16 +64,16 @@ public class TicTacToe {
 
     private static void printMap() {
         int i;
-        for(i = 0; i < 6; ++i) {
+        for (i = 0; i < SIZE + 1; ++i) {
             System.out.print(i + " ");
         }
 
         System.out.println();
 
-        for(i = 0; i < 5; ++i) {
+        for (i = 0; i < SIZE; ++i) {
             System.out.print(i + 1 + " ");
 
-            for(int j = 0; j < 5; ++j) {
+            for (int j = 0; j < SIZE; ++j) {
                 System.out.print(map[i][j] + " ");
             }
 
@@ -89,44 +89,43 @@ public class TicTacToe {
             System.out.println("Введите координаты в формате X Y");
             row = scanner.nextInt() - 1;
             col = scanner.nextInt() - 1;
-        } while(!validTurn(row, col));
+        } while (!validTurn(row, col));
 
-        map[row][col] = 'X';
+        map[row][col] = DOT_X;
     }
 
     public static boolean validTurn(int row, int col) {
-        if (row >= 0 && row <= 5 && col >= 0 && col <= 5) {
-            return map[row][col] == 8226;
+        if (row >= 0 && row <= SIZE && col >= 0 && col <= SIZE) {
+            return map[row][col] == DOT_EMPTY;
         } else {
             return false;
         }
     }
 
     public static void aiTurn() {
-        int[] turn = getWinTurn('O');
+        int[] turn = getWinTurn(DOT_O);
         if (turn[0] == -1) {
-            turn = getWinTurn('X');
+            turn = getWinTurn(DOT_X);
             if (turn[0] == -1) {
                 do {
-                    turn[0] = random.nextInt(5);
-                    turn[1] = random.nextInt(5);
-                } while(!validTurn(turn[0], turn[1]));
+                    turn[0] = random.nextInt(SIZE);
+                    turn[1] = random.nextInt(SIZE);
+                } while (!validTurn(turn[0], turn[1]));
             }
         }
 
         System.out.println(Arrays.toString(turn));
-        map[turn[0]][turn[1]] = 'O';
+        map[turn[0]][turn[1]] = DOT_O;
     }
 
     public static boolean isMapFull() {
-        for(int i = 0; i < 4; ++i) {
-            for(int j = 0; j < 4; ++j) {
-                if (map[i][j] == 8226) {
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                if (map[i][j] == DOT_EMPTY) {
                     return false;
                 }
             }
         }
-
         return true;
     }
 
@@ -135,41 +134,35 @@ public class TicTacToe {
     }
 
     private static boolean checkRowWin(char symb) {
-        for(int i = 0; i < 5; ++i) {
-            int row = 0;
-            int col = 0;
-
-            for(int j = 0; j < 5; ++j) {
+        for (int i = 0; i < SIZE; ++i) {
+            int row = 0, col = 0;
+            for (int j = 0; j < SIZE; ++j) {
                 row = map[i][j] == symb ? row + 1 : 0;
                 col = map[j][i] == symb ? col + 1 : 0;
-                if (row == 4 || col == 4) {
+                if (row == DOTS_TO_WIN || col == DOTS_TO_WIN) {
                     return true;
                 }
             }
         }
-
         return false;
     }
 
     private static boolean checkDiagonalWin(char symb) {
-        int diag1 = 0;
-        int diag2 = 0;
-
-        for(int i = 0; i < 5; ++i) {
+        int diag1 = 0, diag2 = 0;
+        for (int i = 0; i < SIZE; ++i) {
             diag1 = map[i][i] == symb ? diag1 + 1 : 0;
-            diag2 = map[i][5 - i - 1] == symb ? diag2 + 1 : 0;
-            if (diag1 == 4 || diag2 == 4) {
+            diag2 = map[i][SIZE - i - 1] == symb ? diag2 + 1 : 0;
+            if (diag1 == DOTS_TO_WIN || diag2 == DOTS_TO_WIN) {
                 return true;
             }
         }
-
         return false;
     }
 
     private static int[] getWinTurn(char symb) {
-        for(int row = 0; row < 5; ++row) {
-            for(int col = 0; col < 5; ++col) {
-                if (map[row][col] == 8226 && winMove(row, col, symb)) {
+        for (int row = 0; row < SIZE; ++row) {
+            for (int col = 0; col < SIZE; ++col) {
+                if (map[row][col] == DOT_EMPTY && winMove(row, col, symb)) {
                     return new int[]{row, col};
                 }
             }
@@ -181,12 +174,7 @@ public class TicTacToe {
     private static boolean winMove(int row, int col, char symb) {
         map[row][col] = symb;
         boolean result = checkWin(symb);
-        map[row][col] = 8226;
+        map[row][col] = DOT_EMPTY;
         return result;
-    }
-
-    static {
-        scanner = new Scanner(System.in);
-        random = new Random();
     }
 }
